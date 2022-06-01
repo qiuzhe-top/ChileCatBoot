8d22cb2f40786cdeb02c3e58cba8811c72ce38e8
qiuzhe <2810201146@qq.com>
2022-06-01 17:22:55 +0800

    初始化

---
+++ src/main/java/top/ccat/ChileCatBootApplication.java
@@ -0,0 +1,13 @@
+package top.ccat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChileCatBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChileCatBootApplication.class, args);
+    }
+
+}
